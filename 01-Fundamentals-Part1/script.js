6d398e862b417712357dc266d206fd1526e37172
Nita Constantin <ctinnita88@gmail.com>
2023-08-14 23:11:25 +0300

    02 js fundamentals - part 1: values and variables

--- 01-Fundamentals-Part1/script.js
+++ 01-Fundamentals-Part1/script.js
@@ -1,4 +1,27 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
-
+// if (js === "amazing") alert("JavaScript is FUN!");
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firtsName = "Matilda";
+
+console.log(firtsName);
+console.log(firtsName);
+console.log(firtsName);
+
+// let 3years = 3;
+// let jonas&Matilda = 'JM';
+// let new = 27
+let firstName = "Jonas";
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirsJob = "Coder";
+let myCurrentJob = "Teacher";
+
+// let job1 = "programmer";
+// let job2 = "teacher";
+
+console.log(myFirsJob);
