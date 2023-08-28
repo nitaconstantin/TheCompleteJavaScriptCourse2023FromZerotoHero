2117eb8d8019a227ad51b77cf35c9442976ddd64
Nita Constantin <ctinnita88@gmail.com>
2023-08-28 16:30:57 +0300

    Sectiunea 2 - lectia 12 let, const and var

--- 01-Fundamentals-Part1/script.js
+++ 01-Fundamentals-Part1/script.js
@@ -1,35 +1,35 @@
 let js = "amazing";
 // if (js === "amazing") alert("JavaScript is FUN!");
-console.log(40 + 8 + 23 - 10);
+// console.log(40 + 8 + 23 - 10);
 
-console.log("Jonas");
-console.log(23);
+// console.log("Jonas");
+// console.log(23);
 
-let firtsName = "Matilda";
+// let firtsName = "Matilda";
 
-console.log(firtsName);
-console.log(firtsName);
-console.log(firtsName);
+// console.log(firtsName);
+// console.log(firtsName);
+// console.log(firtsName);
 
-// let 3years = 3;
-// let jonas&Matilda = 'JM';
-// let new = 27
-let firstName = "Jonas";
-let person = "jonas";
-let PI = 3.1415;
+// // let 3years = 3;
+// // let jonas&Matilda = 'JM';
+// // let new = 27
+// let firstName = "Jonas";
+// let person = "jonas";
+// let PI = 3.1415;
 
-let myFirsJob = "Coder";
-let myCurrentJob = "Teacher";
+// let myFirsJob = "Coder";
+// let myCurrentJob = "Teacher";
 
-// let job1 = "programmer";
-// let job2 = "teacher";
+// // let job1 = "programmer";
+// // let job2 = "teacher";
 
-console.log(myFirsJob);
+// console.log(myFirsJob);
 
-let country = "Romania";
-let continent = "Europe";
-let population = 19859575;
-console.log(country, continent, population);
+// let country = "Romania";
+// let continent = "Europe";
+// let population = 19859575;
+// console.log(country, continent, population);
 
 // Primitive data types
 /*
@@ -42,20 +42,32 @@ console.log(country, continent, population);
 7. BigInt(ES2020): larger integers than the Number type can hold
 */
 
-let javaScriptIsFun = true;
-console.log(javaScriptIsFun);
-console.log(typeof true);
-console.log(typeof javaScriptIsFun);
-// console.log(typeof 23);
-// console.log(typeof "Jonas");
+// let javaScriptIsFun = true;
+// console.log(javaScriptIsFun);
+// console.log(typeof true);
+// console.log(typeof javaScriptIsFun);
+// // console.log(typeof 23);
+// // console.log(typeof "Jonas");
 
-javaScriptIsFun = "YES!";
-console.log(typeof javaScriptIsFun);
+// javaScriptIsFun = "YES!";
+// console.log(typeof javaScriptIsFun);
 
-let year;
-console.log(year);
-console.log(typeof year);
+// let year;
+// console.log(year);
+// console.log(typeof year);
 
-year = 1991;
-console.log(typeof year);
-console.log(typeof null);
+// year = 1991;
+// console.log(typeof year);
+// console.log(typeof null);
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+
+// const job;
+
+var job = "programmer";
+job = "teacher";
+lastName = "Schmedtmann";
+console.log(lastName);
