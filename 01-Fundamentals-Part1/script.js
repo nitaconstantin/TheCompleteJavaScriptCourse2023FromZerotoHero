779ec343e4b800c371b6fd3b4aaf4723228074c1
Nita Constantin <ctinnita88@gmail.com>
2023-08-28 17:55:06 +0300

    Sectiunea 2 - Lectia 13 - basic operators

--- 01-Fundamentals-Part1/script.js
+++ 01-Fundamentals-Part1/script.js
@@ -59,15 +59,47 @@ let js = "amazing";
 // year = 1991;
 // console.log(typeof year);
 // console.log(typeof null);
-let age = 30;
-age = 31;
+// let age = 30;
+// age = 31;
 
-const birthYear = 1991;
-// birthYear = 1990;
+// const birthYear = 1991;
+// // birthYear = 1990;
 
-// const job;
+// // const job;
 
-var job = "programmer";
-job = "teacher";
-lastName = "Schmedtmann";
-console.log(lastName);
+// var job = "programmer";
+// job = "teacher";
+// lastName = "Schmedtmann";
+// console.log(lastName);
+
+// BASIC OPERATORS
+/*
+const ageJonas = 2037-1997
+*/
+// Math Operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2020;
+console.log(ageJonas, ageSarah);
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2**3 means 2 to the power of 3 = 2*2*2
+
+const firstName = "Jonas";
+const lastName = "Schmedtmann";
+console.log(firstName + " " + lastName);
+
+// Assignment Operators
+let x = 10 + 5;
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1;
+x--;
+x--;
+console.log(x);
+
+// Comparison Operators
+console.log(ageJonas > ageSarah); // > , <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+console.log(now - 1991 > now - 2018);
