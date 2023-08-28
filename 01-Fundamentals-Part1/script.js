dfab3dfe1adeb42b86958e9c908c3da9a61ac4b1
Nita Constantin <ctinnita88@gmail.com>
2023-08-28 14:26:28 +0300

    Sectiunea 2 - lectia 11

--- 01-Fundamentals-Part1/script.js
+++ 01-Fundamentals-Part1/script.js
@@ -30,3 +30,32 @@ let country = "Romania";
 let continent = "Europe";
 let population = 19859575;
 console.log(country, continent, population);
+
+// Primitive data types
+/*
+1. Number: ex: let age = 23
+2. String: ex: let firtsName = 'Jonas';
+3. Boolean ex: let fullAge = true;
+4. Undefined ex: let children;
+5. Null: Also means 'empty value';
+6. Symbol(ES2015): value that is unique and cannot be changed;
+7. BigInt(ES2020): larger integers than the Number type can hold
+*/
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Jonas");
+
+javaScriptIsFun = "YES!";
+console.log(typeof javaScriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+console.log(typeof null);
