20e83e8e1b6a418c97b7e270e36e9593cf00562d
Nita Constantin <ctinnita88@gmail.com>
2023-08-31 14:16:49 +0300

    sectiunea 2 - 17. Strings and Template Literals

--- 01-Fundamentals-Part1/script.js
+++ 01-Fundamentals-Part1/script.js
@@ -105,15 +105,39 @@ const ageJonas = 2037-1997
 // console.log(now - 1991 > now - 2018);
 
 // Operator Precedence
-const now = 2037;
-const ageJonas = now - 1991;
-const ageSarah = now - 2018;
-console.log(now - 1991 > now - 2018);
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(now - 1991 > now - 2018);
 
-// console.log(25 - 10 - 5);
+// // console.log(25 - 10 - 5);
 
-let x, y;
-x = y = 25 - 10 - 5; // x = y = 10
-console.log(x, y);
-const averageAge = (ageJonas + ageSarah) / 2;
-console.log(ageJonas, ageSarah, averageAge);
+// let x, y;
+// x = y = 25 - 10 - 5; // x = y = 10
+// console.log(x, y);
+// const averageAge = (ageJonas + ageSarah) / 2;
+// console.log(ageJonas, ageSarah, averageAge);
+
+const firstName = "Jonas";
+const job = "teacher";
+const birthYear = 1991;
+const year = 2037;
+
+const jonas =
+  "I'm " + firstName + ", a " + (year - birthYear) + " years old " + job + "!";
+
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName} a ${year - birthYear} years old ${job} !`;
+console.log(jonasNew);
+
+console.log(`Jonas a regular string ...`);
+// console.log('String with \n\
+// multiple \n\
+// lines
+// ');
+
+console.log(`String with
+multiple
+lines
+`);
