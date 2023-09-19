262db626b906e7b50b2d688c3c4894af5104035c
Nita Constantin <ctinnita88@gmail.com>
2023-09-19 19:34:55 +0300

    section 3 - 35. Arrow Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -34,18 +34,33 @@
 // const num = Number("23");
 // console.log(num);
 
-// Function Declaration:
-function calculateAge1(birthYear) {
-  const age = 2037 - birthYear;
-  return age;
-}
+// // Function Declaration:
+// function calculateAge1(birthYear) {
+//   const age = 2037 - birthYear;
+//   return age;
+// }
 
-const age1 = calculateAge1(1991);
-console.log(age1);
+// const age1 = calculateAge1(1991);
+// console.log(age1);
 
 // Function Expression:
-const calcAge2 = function (birthYear) {
-  return 2037 - birthYear;
+// const calcAge2 = function (birthYear) {
+//   return 2037 - birthYear;
+// };
+// const age2 = calcAge2(1991);
+// console.log(age2);
+
+// Arrow function:
+const calcAge3 = (birthYear) => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  //   return retirement;
+  return `${firstName} retires in ${retirement} years.`;
 };
-const age2 = calcAge2(1991);
-console.log(age2);
+
+console.log(yearsUntilRetirement(1991, "Jonas"));
+console.log(yearsUntilRetirement(1980, "Bob"));
