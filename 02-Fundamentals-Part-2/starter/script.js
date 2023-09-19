0b03cc910416fe209f7a1d16175337692132cc09
Nita Constantin <ctinnita88@gmail.com>
2023-09-19 19:04:57 +0300

    section 3 - Miniatura lecției 19:07 / 19:08 33. Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,11 +1,35 @@
 "use strict";
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log("I can drive :D");
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log("I can drive :D");
 
-// const interface = "Audio";
-// const private = 534;
-// const if = 23;
+// // const interface = "Audio";
+// // const private = 534;
+// // const if = 23;
+
+// Functions in JavaScript
+
+// function logger() {
+//   console.log("My name is Jonas");
+// }
+// // calling / running / invoking the function
+// logger();
+// logger();
+// logger();
+
+function fruitProcessor(apples, oranges) {
+  //   console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+  return juice;
+}
+const appleJuice = fruitProcessor(5, 0);
+console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number("23");
+console.log(num);
