4a160187a3dda07a46db9f306b3b45f6b96dffa0
Nita Constantin <ctinnita88@gmail.com>
2023-09-19 14:00:46 +0300

    section 2 - Exercițiu de codare 4: CHALLENGE #4

---
+++ 01-Fundamentals-Part1/challenge4.js
@@ -0,0 +1,6 @@
+const bill = 430;
+const tip = bill >= 50 && bill <= 300 ? (bill * 15) / 100 : (bill * 20) / 100;
+const totalVal = bill + tip;
+console.log(
+  `The bill was ${bill}, the tip was ${tip}, and the total value ${totalVal}`
+);
