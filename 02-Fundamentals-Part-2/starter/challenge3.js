b3b2539bebbc9053e8a59942c007bbe46491b68d
Nita Constantin <ctinnita88@gmail.com>
2023-09-22 20:06:27 +0300

    section 3 - Exercițiu de codare 7: CHALLENGE #3

---
+++ 02-Fundamentals-Part-2/starter/challenge3.js
@@ -0,0 +1,33 @@
+const mark = {
+  fullName: "Mark Miller",
+  lastName: "Miller",
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  },
+};
+
+const john = {
+  fullName: "John Smith",
+  lastName: "Smith",
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  },
+};
+
+mark.calcBMI() > john.calcBMI()
+  ? console.log(
+      `${mark.fullName}'s BMI (${mark.calcBMI()}) is higher than ${
+        john.fullName
+      }'s (${john.calcBMI()})`
+    )
+  : console.log(
+      `${john.fullName}'s BMI (${john.calcBMI()}) is higher than ${
+        mark.fullName
+      }'s (${mark.calcBMI()}) `
+    );
