94bf845b73e3b135b6aa550d255d3131ce7595d2
Nita Constantin <ctinnita88@gmail.com>
2023-09-22 18:16:03 +0300

    section 3 - 44. Object Methods

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -177,28 +177,28 @@
 //   ["Michael", "Peter", "Steven"],
 // ];
 
-const jonas = {
-  firstName: "Jonas",
-  lastName: "Schmedtmann",
-  age: 2037 - 1991,
-  profession: "teacher",
-  friends: ["Michael", "Peter", "Steven"],
-};
+// const jonas = {
+//   firstName: "Jonas",
+//   lastName: "Schmedtmann",
+//   age: 2037 - 1991,
+//   profession: "teacher",
+//   friends: ["Michael", "Peter", "Steven"],
+// };
 
-console.log(jonas);
-console.log(jonas.lastName);
-console.log(jonas["lastName"]);
+// console.log(jonas);
+// console.log(jonas.lastName);
+// console.log(jonas["lastName"]);
 
-const nameKey = "Name";
-console.log(jonas["first" + nameKey]);
-console.log(jonas["last" + nameKey]);
+// const nameKey = "Name";
+// console.log(jonas["first" + nameKey]);
+// console.log(jonas["last" + nameKey]);
 
-// console.log(jonas.'last' + nameKey)
+// // console.log(jonas.'last' + nameKey)
 
-const interestedIn = prompt(
-  "What do you want to know about Jonas? Choose between firstName, lastName, age, profession and friends."
-);
-console.log(interestedIn);
+// const interestedIn = prompt(
+//   "What do you want to know about Jonas? Choose between firstName, lastName, age, profession and friends."
+// );
+// console.log(interestedIn);
 
 // switch (interestedIn) {
 //   case "firstName":
@@ -221,20 +221,58 @@ console.log(interestedIn);
 //     break;
 // }
 
-if (jonas[interestedIn]) {
-  console.log(jonas[interestedIn]);
-} else {
-  console.log(
-    "Wrong request! Choose between firstName, lastName, age, profession and friends"
-  );
-}
+// if (jonas[interestedIn]) {
+//   console.log(jonas[interestedIn]);
+// } else {
+//   console.log(
+//     "Wrong request! Choose between firstName, lastName, age, profession and friends"
+//   );
+// }
 
-jonas.location = "Portugal";
-jonas["twitter"] = "@jonasscmedtman";
-console.log(jonas);
+// jonas.location = "Portugal";
+// jonas["twitter"] = "@jonasscmedtman";
+// console.log(jonas);
+
+// // Challenge
+// // 'Jonas has 3 friends, and his best friend is Michael
+// console.log(
+//   `${jonas.firstName} has  ${jonas.friends.length} friends, and his best friend is ${jonas.friends[0]}`
+// );
+
+const jonas = {
+  firstName: "Jonas",
+  lastName: "Schmedtmann",
+  birthYear: 1991,
+  profession: "teacher",
+  friends: ["Michael", "Peter", "Steven"],
+  hasDriverLicense: true,
+  //   calcAge: function () {
+  //     return 2037 - this.birthYear;
+  //   },
+
+  calcAge: function () {
+    this.age = 2037 - this.birthYear;
+    return this.age;
+  },
+  getSummary: function () {
+    return `${this.firstName} is a ${this.calcAge()} - years old ${
+      this.profession
+    } and  he ${this.hasDriverLicense ? "has a " : "has no "} driver's license`;
+  },
+};
+// const calcAge = function (birthYear) {
+//   return 2037 - birthYear;
+// };
+
+console.log(jonas.calcAge());
+// console.log(jonas["calcAge"](jonas["birthYear"]));
+
+// console.log(jonas.calcAge());
+// console.log(jonas.calcAge());
+// console.log(jonas.calcAge());
+console.log(jonas.age);
 
 // Challenge
-// 'Jonas has 3 friends, and his best friend is Michael
-console.log(
-  `${jonas.firstName} has  ${jonas.friends.length} friends, and his best friend is ${jonas.friends[0]}`
-);
+// Jonas is a 46-year old teacher, and he has a / not a drivers's license
+
+console.log(jonas.getSummary());
