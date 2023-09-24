3387ee11852f1aea484ed65fbe86a4e22977338c
Nita Constantin <ctinnita88@gmail.com>
2023-09-24 19:14:36 +0300

    section 7 - 72. Selecting and Manipulating Elements

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,2 +1,9 @@
 'use strict';
 console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct number!🎉';
+// console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
