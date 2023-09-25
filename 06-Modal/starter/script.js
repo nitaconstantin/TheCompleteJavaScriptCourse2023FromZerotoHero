d18a1fab8cabd3e597505e30f13bb3650634b676
Nita Constantin <ctinnita88@gmail.com>
2023-09-25 11:12:57 +0300

    section 7 - 80. Working With Classes

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -6,12 +6,11 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  //   btnsOpenModal[i].addEventListener('click', function () {
-  //     document.querySelector('.modal').classList.remove('hidden');
-  //     document.querySelector('.overlay').classList.remove('hidden');
-
-  //   });
-  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+  //   console.log(btnsOpenModal[i].textContent);
 }
 // modals.forEach(modal => {
 //   modal.addEventListener('click', function () {
@@ -19,3 +18,11 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 //     document.querySelector('.overlay').classList.remove('hidden');
 //   });
 // });
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
