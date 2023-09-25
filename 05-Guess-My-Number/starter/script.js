ec583b20dd071c7694735873e7c77d219d17710f
Nita Constantin <ctinnita88@gmail.com>
2023-09-25 09:51:07 +0300

    section 7 - 77. Implementing Highscores

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -10,6 +10,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 // document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -24,6 +25,10 @@ document.querySelector('.check').addEventListener('click', function () {
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
     // when guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
@@ -57,6 +62,4 @@ document.querySelector('.again').addEventListener('click', function () {
 
   document.querySelector('.guess').value = '';
   document.querySelector('body').style.backgroundColor = '#222';
-
-  console.log(score);
 });
