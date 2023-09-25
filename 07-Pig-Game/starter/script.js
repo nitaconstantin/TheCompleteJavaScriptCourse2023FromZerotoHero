c716dad15f62e88259467076577b7984103092bf
Nita Constantin <ctinnita88@gmail.com>
2023-09-25 16:55:12 +0300

    section 7 - 86. Resetting the Game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -3,18 +3,34 @@ const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
-const current0El = document.getElementById('current--0');
-const current1El = document.getElementById('current--1');
+let current0El = document.getElementById('current--0');
+let current1El = document.getElementById('current--1');
+let scores, currentScore, activePlayer, playing;
 
 const diceEl = document.querySelector('.dice');
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function () {
+  diceEl.classList.add('hidden');
+
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  //   document
+  //     .querySelector(`.player--${activePlayer}`)
+  //     .classList.remove('player--winner');
+  //   document.querySelector(`.player--0`).classList.add('player--active');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+init();
+
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   currentScore = 0; // change later
@@ -26,6 +42,7 @@ const switchPlayer = function () {
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+
 const rollingTheDice = function () {
   if (playing) {
     // 1. generating a random dice roll
@@ -66,7 +83,7 @@ btnHold.addEventListener('click', function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
     // 2. Check if player 's score is >= 100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       playing = false;
       diceEl.classList.add('hidden');
 
@@ -83,3 +100,5 @@ btnHold.addEventListener('click', function () {
     // Switch to the next player
   }
 });
+
+btnNew.addEventListener('click', init);
