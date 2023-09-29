800163114257400cb9ced5a0a865f2b76dd7fb2c
Nita Constantin <ctinnita88@gmail.com>
2023-09-30 00:24:55 +0300

    section 9 - 113. Optional Chaining (?.)

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -51,7 +51,26 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
+if (restaurant.openingHours.mon) console.log(restaurant.openingHours.mon.open);
 
+// With optional chaining
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+// Example:
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+for (let day of days) {
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at  ${open}`);
+}
+
+// Methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRissotto?.(0, 1) ?? 'Method does not exist');
+// Arrays
+let users = [{ name: 'Jonas', email: 'hello@jonas.io' }];
+users = [];
+console.log(users[0]?.name ?? 'User array empty');
 // const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 // for (const item of menu) {
 //   console.log(item);
