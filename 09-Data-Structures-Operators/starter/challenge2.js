31b8d649031e827e5bb39e6a3ccfe17d68a0947b
Nita Constantin <ctinnita88@gmail.com>
2023-10-01 18:22:34 +0300

    section 9 - 115. Coding Challenge #2

---
+++ 09-Data-Structures-Operators/starter/challenge2.js
@@ -0,0 +1,65 @@
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnabry',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnabry', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// for (let g of game.scored) {
+//   console.log(g);
+// }
+
+// 1.
+for (const [i, player] of game.scored.entries()) {
+  console.log(`Goal ${i + 1}: ${player}`);
+}
+
+// 2.
+let average = 0;
+const odds = Object.values(game.odds);
+for (const odd of odds) {
+  average += odd;
+}
+average /= odds.length;
+console.log(average);
+
+// 3.
+for (const [team, odd] of Object.entries(game.odds)) {
+  //   console.log(team, odd);
+  const teamStr = team == 'x' ? 'draw' : `victori ${game[team]}`;
+  console.log(`Odd of victory ${teamStr}: ${odd}`);
+}
