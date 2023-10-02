b31c8b6b4374051e2e4dde30a822af906ca7ae70
Nita Constantin <ctinnita88@gmail.com>
2023-10-02 12:18:58 +0300

    118. Maps: Iteration

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -53,34 +53,63 @@ const restaurant = {
 };
 
 // Maps
-const rest = new Map();
-rest.set('name', 'Classico Italiano');
-rest.set(1, 'Firenze', 'Italy');
-rest.set(2, 'Lisbon', 'Portugal');
-console.log(rest.set(2, 'Lisbon Portugal'));
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct 🎉'],
+  [false, 'Try again '],
+]);
+console.log(question);
 
-rest
-  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
-  .set('open', 11)
-  .set('close', 23)
-  .set(true, 'We are open :D')
-  .set(false, 'We are closed :(');
+// Quiz app
+console.log(question.get('question'));
+// iteration
+for (const [key, value] of question) {
+  if (typeof key === 'number') {
+    console.log(`Answer ${key}: ${value}`);
+  }
+}
+// const answer = Number(prompt('Your answer'));
+// console.log(answer);
 
-console.log(rest.get('name'));
-// console.log(rest.get(true));
+// console.log(question.get(question.get('correct') === answer));
 
-const time = 21;
-console.log(rest.get(time > rest.get(open) && time < rest.get(close)));
+// Convert Map to Array
+console.log([...question]);
+console.log(question.entries());
+console.log([...question.keys()]);
+console.log(question.values());
+// const rest = new Map();
+// rest.set('name', 'Classico Italiano');
+// rest.set(1, 'Firenze', 'Italy');
+// rest.set(2, 'Lisbon', 'Portugal');
+// console.log(rest.set(2, 'Lisbon Portugal'));
 
-console.log(rest.has('categories'));
-rest.delete(2);
-console.log(rest);
-console.log(rest.size);
-// rest.clear();
-rest.set([1, 2], 'Test');
-console.log(rest);
-console.log(rest.size);
-console.log(rest.get([1, 2]));
+// rest
+//   .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+//   .set('open', 11)
+//   .set('close', 23)
+//   .set(true, 'We are open :D')
+//   .set(false, 'We are closed :(');
+
+// console.log(rest.get('name'));
+// // console.log(rest.get(true));
+
+// const time = 21;
+// console.log(rest.get(time > rest.get(open) && time < rest.get(close)));
+
+// console.log(rest.has('categories'));
+// rest.delete(2);
+// console.log(rest);
+// console.log(rest.size);
+// // rest.clear();
+// rest.set([1, 2], 'Test');
+// console.log(rest);
+// console.log(rest.size);
+// console.log(rest.get([1, 2]));
 
 // SETS
 // const ordersSet = new Set([
