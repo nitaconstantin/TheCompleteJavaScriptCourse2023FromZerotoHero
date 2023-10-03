e40f8cf3e2ae46b4a3ed0324030272416d0ec470
Nita Constantin <ctinnita88@gmail.com>
2023-10-03 16:14:27 +0300

    section 10 - 132. Functions Returning Functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -56,27 +56,46 @@
 // newPassport(jonas);
 // checkIn(flight, jonas);
 
-const oneWord = function (str) {
-  return str.replace(/ /g, '').toLowerCase();
-};
-const upperFirstWord = function (str) {
-  const [first, ...others] = str.split(' ');
-  return [first.toUpperCase(), ...others].join(' ');
+// const oneWord = function (str) {
+//   return str.replace(/ /g, '').toLowerCase();
+// };
+// const upperFirstWord = function (str) {
+//   const [first, ...others] = str.split(' ');
+//   return [first.toUpperCase(), ...others].join(' ');
+// };
+
+// // Higher-order function
+// const transformer = function (str, fn) {
+//   console.log(`Original string ${str}`);
+//   console.log(`Transformed string : ${fn(str)}`);
+//   console.log(`Transformed by:  ${fn.name}`);
+// };
+
+// transformer('JavaScript is the best!', upperFirstWord);
+// transformer('JavaScript is the best!', oneWord);
+
+// const high5 = function () {
+//   console.log('👋');
+// };
+// document.body.addEventListener('click', high5);
+
+// ['Jonas', 'Martha', 'Adam'].forEach(high5);
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
 };
 
-// Higher-order function
-const transformer = function (str, fn) {
-  console.log(`Original string ${str}`);
-  console.log(`Transformed string : ${fn(str)}`);
-  console.log(`Transformed by:  ${fn.name}`);
+const greet2 = greeting2 => {
+  return name2 => {
+    console.log(`${greeting2} ${name2}`);
+  };
 };
 
-transformer('JavaScript is the best!', upperFirstWord);
-transformer('JavaScript is the best!', oneWord);
-
-const high5 = function () {
-  console.log('👋');
-};
-document.body.addEventListener('click', high5);
-
-['Jonas', 'Martha', 'Adam'].forEach(high5);
+const greeterHey = greet('Hey');
+greeterHey('Jonas');
+greeterHey('Steven');
+greet('Hello')('Jonas');
+const greetHi = greet2('Hi');
+greetHi('Matilda');
