1119786c3e1f50e5b720b8822e90d295b79c4016
Nita Constantin <ctinnita88@gmail.com>
2023-10-03 16:03:23 +0300

    section 10 - 131. Functions Accepting Callback Functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -25,33 +25,58 @@
 
 // 129. How Passing Arguments Works: Value vs. Reference
 
-const flight = 'LH234';
-const jonas = {
-  name: 'Jonas Schmedtmann',
-  passport: 24739479284,
+// const flight = 'LH234';
+// const jonas = {
+//   name: 'Jonas Schmedtmann',
+//   passport: 24739479284,
+// };
+
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = 'LH999';
+//   passenger.name = 'Mr. ' + passenger.name;
+
+//   if (passenger.passport === 24739479284) {
+//     alert('Check in');
+//   } else {
+//     alert('Wrong passport!');
+//   }
+// };
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// const flightNum = flight;
+// const passenger = jonas;
+
+// const newPassport = function (person) {
+//   person.passport = Math.trunc(Math.random() * 1000000000);
+// };
+
+// newPassport(jonas);
+// checkIn(flight, jonas);
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
 };
 
-const checkIn = function (flightNum, passenger) {
-  flightNum = 'LH999';
-  passenger.name = 'Mr. ' + passenger.name;
-
-  if (passenger.passport === 24739479284) {
-    alert('Check in');
-  } else {
-    alert('Wrong passport!');
-  }
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string ${str}`);
+  console.log(`Transformed string : ${fn(str)}`);
+  console.log(`Transformed by:  ${fn.name}`);
 };
 
-checkIn(flight, jonas);
-console.log(flight);
-console.log(jonas);
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
 
-const flightNum = flight;
-const passenger = jonas;
-
-const newPassport = function (person) {
-  person.passport = Math.trunc(Math.random() * 1000000000);
+const high5 = function () {
+  console.log('👋');
 };
+document.body.addEventListener('click', high5);
 
-newPassport(jonas);
-checkIn(flight, jonas);
+['Jonas', 'Martha', 'Adam'].forEach(high5);
