489458850dd82caac96e4b9ec12e670acb0797fe
Nita Constantin <ctinnita88@gmail.com>
2023-10-03 10:26:41 +0300

    section 9 - 129. How Passing Arguments Works: Value vs. Reference

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,24 +1,57 @@
 'use strict';
 
-const bookings = [];
-const createBooking = function (
-  flightNumber,
-  numPassengers = 1,
-  price = 199 * numPassengers
-) {
-  // ES5
-  //   numPassengers = numPassengers || 1;
-  //   price = price || 199;
-  const booking = {
-    flightNumber,
-    numPassengers,
-    price,
-  };
-  console.log(booking);
-  bookings.push(booking);
-};
-createBooking('LH123');
-createBooking('LH123', 2, 800);
-createBooking('LH123', 5);
+// const bookings = [];
+// const createBooking = function (
+//   flightNumber,
+//   numPassengers = 1,
+//   price = 199 * numPassengers
+// ) {
+//   // ES5
+//   //   numPassengers = numPassengers || 1;
+//   //   price = price || 199;
+//   const booking = {
+//     flightNumber,
+//     numPassengers,
+//     price,
+//   };
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+// createBooking('LH123');
+// createBooking('LH123', 2, 800);
+// createBooking('LH123', 5);
 
-createBooking('LH123', undefined, 1000);
+// createBooking('LH123', undefined, 1000);
+
+// 129. How Passing Arguments Works: Value vs. Reference
+
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schmedtmann',
+  passport: 24739479284,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 24739479284) {
+    alert('Check in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+checkIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
+
+const flightNum = flight;
+const passenger = jonas;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 1000000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
