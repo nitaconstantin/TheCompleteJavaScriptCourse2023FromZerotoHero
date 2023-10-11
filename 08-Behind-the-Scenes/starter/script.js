183d3d58ed4be8aaba44d95bceeabbcd677b5b92
Nita Constantin <ctinnita88@gmail.com>
2023-10-11 12:32:50 +0300

    section 11 - 142. Simple Array Methods

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -167,36 +167,36 @@
 // console.log('Me: ', me);
 
 // Primitive types
-let lastName = 'Williams';
-let oldLastName = lastName;
-lastName = 'Davis';
+// let lastName = 'Williams';
+// let oldLastName = lastName;
+// lastName = 'Davis';
 
-console.log(lastName, oldLastName);
-// Refrence types
-const jessica = {
-  firstName: 'Jessica',
-  lastName: 'Williams',
-  age: 27,
-};
+// console.log(lastName, oldLastName);
+// // Refrence types
+// const jessica = {
+//   firstName: 'Jessica',
+//   lastName: 'Williams',
+//   age: 27,
+// };
 
-const marriedJessica = jessica;
-marriedJessica.lastName = 'Davis';
-console.log('Before marriage:', jessica);
-console.log('After marriage:', marriedJessica);
+// const marriedJessica = jessica;
+// marriedJessica.lastName = 'Davis';
+// console.log('Before marriage:', jessica);
+// console.log('After marriage:', marriedJessica);
 
-// marriedJessica = {};
-// Copying objects
-const jessica2 = {
-  firstName: 'Jessica',
-  lastName: 'Williams',
-  age: 27,
-  family: ['Alice', 'Bob'],
-};
+// // marriedJessica = {};
+// // Copying objects
+// const jessica2 = {
+//   firstName: 'Jessica',
+//   lastName: 'Williams',
+//   age: 27,
+//   family: ['Alice', 'Bob'],
+// };
 
-const jessicaCopy = Object.assign({}, jessica2);
-jessicaCopy.lastName = 'Davis';
+// const jessicaCopy = Object.assign({}, jessica2);
+// jessicaCopy.lastName = 'Davis';
 
-jessicaCopy.family.push('Mary');
-jessicaCopy.family.push('John');
-console.log('Before marriage:', jessica2);
-console.log('After marriage:', jessicaCopy);
+// jessicaCopy.family.push('Mary');
+// jessicaCopy.family.push('John');
+// console.log('Before marriage:', jessica2);
+// console.log('After marriage:', jessicaCopy);
