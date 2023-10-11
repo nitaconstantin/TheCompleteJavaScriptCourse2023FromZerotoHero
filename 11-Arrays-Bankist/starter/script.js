9595f6d3ffd9737d9e2e38dfd4a0b8e0245e89ba
Nita Constantin <ctinnita88@gmail.com>
2023-10-11 13:04:21 +0300

    section 11 - 144. Looping Arrays: forEach

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -75,6 +75,22 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
+// for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1} You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1} You withdrew ${Math.abs(movement)}`);
+  }
+}
+console.log('------------FOREACH------------');
+movements.forEach(function (movement, index, array) {
+  if (movement > 0) {
+    console.log(`Movement ${index + 1} You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${index + 1}You withdrew ${Math.abs(movement)}`);
+  }
+});
 // let arr = ['a', 'b', 'c', 'd', 'e'];
 // console.log(arr.slice(2));
 // console.log(arr.slice(2, 4));
@@ -106,12 +122,12 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // // JOIN
 // console.log(letters.join('-'));
 
-const arr = [23, 11, 64];
-console.log(arr[0]);
-console.log(arr.at(0));
+// const arr = [23, 11, 64];
+// console.log(arr[0]);
+// console.log(arr.at(0));
 
-console.log(arr[arr.length - 1]);
-console.log(arr.slice(-1)[0]);
-console.log(arr.at(-1));
+// console.log(arr[arr.length - 1]);
+// console.log(arr.slice(-1)[0]);
+// console.log(arr.at(-1));
 
-console.log('jonas'.at(-1));
+// console.log('jonas'.at(-1));
