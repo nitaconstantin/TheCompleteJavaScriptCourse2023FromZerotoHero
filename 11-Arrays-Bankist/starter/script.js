830e04f28048fced3c2efdfe6355a45256b09dde
Nita Constantin <ctinnita88@gmail.com>
2023-10-19 17:45:31 +0300

    section 11 - 159. Implementing Transfers

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -79,9 +79,9 @@ const displayMovements = function (movements) {
 
 // displayMovements(account1.movements);
 
-const calcDisplayBalance = function (movements) {
-  const balance = movements.reduce((acc, mov) => (acc += mov), 0);
-  labelBalance.textContent = `${balance}€`;
+const calcDisplayBalance = function (acc) {
+  acc.balance = acc.movements.reduce((acc, mov) => (acc += mov), 0);
+  labelBalance.textContent = `${acc.balance}€`;
 };
 
 //calcDisplayBalance(account1.movements);
@@ -122,6 +122,17 @@ const createUsernames = function (accs) {
 };
 createUsernames(accounts); //  stw
 
+const updateUI = function (acc) {
+  //Display movements
+  displayMovements(acc.movements);
+
+  // Display balance
+  calcDisplayBalance(acc);
+
+  // Display summary
+  calcDisplaySummary(acc);
+};
+
 // Event handler
 let currentAccount;
 btnLogin.addEventListener('click', function (e) {
@@ -144,14 +155,38 @@ btnLogin.addEventListener('click', function (e) {
     inputLoginPin.value = '';
     inputLoginUsername.value = '';
     inputLoginPin.blur();
-    //Display movements
-    displayMovements(currentAccount.movements);
+    // //Display movements
+    // displayMovements(currentAccount.movements);
 
-    // Display balance
-    calcDisplayBalance(currentAccount.movements);
+    // // Display balance
+    // calcDisplayBalance(currentAccount);
 
-    // Display summary
-    calcDisplaySummary(currentAccount);
+    // // Display summary
+    // calcDisplaySummary(currentAccount);
+    updateUI(currentAccount);
+  }
+});
+
+btnTransfer.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = Number(inputTransferAmount.value);
+  const receiverAcc = accounts.find(
+    acc => acc.username === inputTransferTo.value
+  );
+
+  inputTransferAmount.value = inputTransferTo.value = '';
+  if (
+    amount > 0 &&
+    receiverAcc &&
+    currentAccount.balance >= amount &&
+    receiverAcc?.username !== currentAccount.username
+  ) {
+    // console.log('Transfer valid!');
+    // Doing the transfer
+    currentAccount.movements.push(-amount);
+    receiverAcc.movements.push(amount);
+    // UPDATE UI
+    updateUI(currentAccount);
   }
 });
 
