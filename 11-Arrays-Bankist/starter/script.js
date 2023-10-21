64ae46014a037922ee7112e72f4f7b9d66807748
Nita Constantin <ctinnita88@gmail.com>
2023-10-21 20:28:19 +0300

    section 11 - 161. some and every

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -190,6 +190,19 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = Number(inputLoanAmount.value);
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    // Add movement
+    currentAccount.movements.push(amount);
+
+    // Update UI
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
+});
+
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
   if (
@@ -383,4 +396,21 @@ const eurToUsd = 1.1;
 // console.log(accounts);
 
 // const account = accounts.find(acc => acc.owner === 'Jessica Davis');
-// console.log(account);
+// // console.log(account);
+// console.log(movements);
+// // Equality
+// console.log(movements.includes(-130));
+
+// // Condition
+// const deposits = movements.some(mov => mov > 1500);
+// console.log(deposits);
+
+// EVERY
+console.log(movements.every(mov => mov > 0));
+console.log(account4.movements.every(mov => mov > 0));
+
+// Separate callback
+const deposit = mov => mov < 0;
+console.log(movements.some(deposit));
+console.log(movements.every(deposit));
+console.log(movements.filter(deposit));
