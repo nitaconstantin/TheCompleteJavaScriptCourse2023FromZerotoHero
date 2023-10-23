e7954e0e5f181070f4ec0fe56e2c404adef52183
Nita Constantin <ctinnita88@gmail.com>
2023-10-23 17:22:20 +0300

    section 12 - 173. Numeric Separators

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -313,24 +313,41 @@ btnSort.addEventListener('click', function (e) {
 // console.log((2.7).toFixed(3));
 // console.log(+(2.354).toFixed(2));
 
-console.log(5 % 2);
-console.log(5 / 2);
-console.log(8 % 3);
-console.log(8 / 3); // 8 = 2 *3 + 2
+// console.log(5 % 2);
+// console.log(5 / 2);
+// console.log(8 % 3);
+// console.log(8 / 3); // 8 = 2 *3 + 2
 
-console.log(7 % 2);
-console.log(7 / 2);
+// console.log(7 % 2);
+// console.log(7 / 2);
 
-const isEven = n => n % 2 === 0;
-console.log(isEven(7));
+// const isEven = n => n % 2 === 0;
+// console.log(isEven(7));
 
-labelBalance.addEventListener('click', function () {
-  [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
-    if (i % 2 === 0) {
-      row.style.backgroundColor = 'orangered';
-    }
-    if (i % 3 === 0) {
-      row.style.backgroundColor = 'blue';
-    }
-  });
-});
+// labelBalance.addEventListener('click', function () {
+//   [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+//     if (i % 2 === 0) {
+//       row.style.backgroundColor = 'orangered';
+//     }
+//     if (i % 3 === 0) {
+//       row.style.backgroundColor = 'blue';
+//     }
+//   });
+// });
+
+//287, 460, 000, 000
+const diameter = 287_460_000_000;
+
+console.log(diameter);
+
+const price = 345_99;
+console.log(price);
+
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+
+const PI = 3.14_15;
+console.log(PI);
+
+console.log(Number('230_000'));
+console.log(parseInt('230_000'));
