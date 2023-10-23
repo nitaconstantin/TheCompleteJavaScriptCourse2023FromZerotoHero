ec4b18da696ec53f51802521e313d6b3b960d210
Nita Constantin <ctinnita88@gmail.com>
2023-10-23 22:47:17 +0300

    section 12 - 175. Creating Dates

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -352,32 +352,63 @@ btnSort.addEventListener('click', function (e) {
 // console.log(Number('230_000'));
 // console.log(parseInt('230_000'));
 
-console.log(2 ** 53 - 1);
-console.log(Number.MAX_SAFE_INTEGER);
-console.log(2 ** 53 + 1);
-console.log(2 ** 53 + 2);
-console.log(2 ** 53 + 3);
-console.log(2 ** 53 + 4);
+// console.log(2 ** 53 - 1);
+// console.log(Number.MAX_SAFE_INTEGER);
+// console.log(2 ** 53 + 1);
+// console.log(2 ** 53 + 2);
+// console.log(2 ** 53 + 3);
+// console.log(2 ** 53 + 4);
 
-console.log(545343423424242435353424523462423907086959n);
-console.log(BigInt(54534342));
+// console.log(545343423424242435353424523462423907086959n);
+// console.log(BigInt(54534342));
 
-// Operations
-console.log(10000n + 10000n);
-console.log(353343543435343534773537537673n * 10000n);
-// console.log(Math.sqrt(16n));
+// // Operations
+// console.log(10000n + 10000n);
+// console.log(353343543435343534773537537673n * 10000n);
+// // console.log(Math.sqrt(16n));
 
-const huge = 3232323232323232323234n;
-const num = 23;
-console.log(huge * BigInt(num));
+// const huge = 3232323232323232323234n;
+// const num = 23;
+// console.log(huge * BigInt(num));
 
-console.log(20n > 15);
-console.log(20n === 20);
-console.log(typeof 20n);
-console.log(20n == '20');
+// console.log(20n > 15);
+// console.log(20n === 20);
+// console.log(typeof 20n);
+// console.log(20n == '20');
 
-console.log(huge + ' is Really big!!');
+// console.log(huge + ' is Really big!!');
 
-// Divisions
-console.log(11n / 3n);
-console.log(10 / 3);
+// // Divisions
+// console.log(11n / 3n);
+// console.log(10 / 3);
+
+// Create a date
+// const now = new Date();
+// console.log(now);
+
+// console.log(new Date('Mon Oct 23 2023 22:32:07 GMT+0300'));
+// console.log(new Date('December 24, 2015'));
+// console.log(new Date(account1.movementsDates[0]));
+
+// console.log(new Date(2037, 10, 19, 15, 23, 5));
+// console.log(new Date(2073, 10, 31));
+
+// console.log(new Date(0));
+// console.log(new Date(3 * 24 * 60 * 60 * 1000));
+
+// Working with dates
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+console.log(future.getTime());
+console.log(new Date(2142249780000));
+
+future.setFullYear(2040);
+console.log(future);
