7acc250c03e72295b04bd8d1fc34ddcdc5ee865a
Nita Constantin <ctinnita88@gmail.com>
2023-10-24 23:15:17 +0300

    section 13 - 187. Styles, Attributes and Classes

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -73,3 +73,48 @@ document
     // message.remove();
     message.parentElement.removeChild(message);
   });
+
+// 187. Styles, Attributes and Classes
+// Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.height);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+  Number.parseInt(getComputedStyle(message).height, 10) + 30 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.className);
+
+logo.alt = 'Beautiful minimalist logo';
+
+console.log(logo.getAttribute('designer'));
+logo.setAttribute('company', 'Bankist');
+
+console.log(logo.getAttribute('src'));
+
+const link = document.querySelector('.twitter-link');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+// Data attributes
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add('c');
+logo.classList.remove('c');
+logo.classList.toggle('c');
+logo.classList.contains('c');
+
+// Don't use!!!
+logo.className = 'jonas';
