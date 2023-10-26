3f9ebbd606e020413815a9c2f3916a9456d8282e
Nita Constantin <ctinnita88@gmail.com>
2023-10-26 13:17:00 +0300

    section 13 - 193. DOM Traversing

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -167,7 +167,7 @@ btnScrollTo.addEventListener('click', function (e) {
 });
 
 // 189. Types of Events and Event Handlers
-const h1 = document.querySelector('h1');
+let h1 = document.querySelector('h1');
 
 // h1.addEventListener('mouseenter', function (e) {
 //   alert('addEventListener: Great! You are reading the heading :D');
@@ -214,3 +214,36 @@ const alertH1 = function (e) {
 // });
 
 // 192. Event Delegation: Implementing Page Navigation
+
+// 193. DOM Traversing
+h1 = document.querySelector('h1');
+
+// Going downwards: child
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'orangered';
+
+// Going upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+
+h1.closest('h1').style.background = 'var(--gradient-primary)';
+
+// Going sideways: siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+
+[...h1.parentElement.children].forEach(function (el) {
+  if (el !== h1) {
+    el.style.transform = 'scale(0.5)';
+  }
+});
