3105ec6eb113774921f2d83c28c42ec6a37d432e
Nita Constantin <ctinnita88@gmail.com>
2023-11-08 13:56:30 +0200

    section 14 - 217. Coding Challenge #2

---
+++ 14-OOP/starter/challenge2.js
@@ -0,0 +1,32 @@
+'use strict';
+
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+  }
+
+  break() {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+  }
+
+  get speedUs() {
+    return this.speed / 1.6;
+  }
+
+  set speedUs(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+const ford = new CarCl('Ford', 120);
+console.log(ford.speedUs);
+ford.accelerate();
+ford.speedUs = 50;
+console.log(ford);
